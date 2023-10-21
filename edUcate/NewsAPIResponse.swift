8b0ce95850e9638a3036d2ad5ee632b6bd062db8
Banah Abdeljaber <babdeljaber14@gmail.com>
2023-10-21 15:39:10 -0500

    Added as files and not folders

---
+++ edUcate/NewsAPIResponse.swift
@@ -0,0 +1,18 @@
+//
+//  NewsAPIResponse.swift
+//  edUcate
+//
+//  Created by Banah Daniella Abdeljaber on 10/21/23.
+//
+
+import Foundation
+//test
+struct NewsAPIResponse: Decodable {
+    
+    let status: String
+    let totalResults: Int?
+    let articles: [Article]?
+    
+    let code: String?
+    let message: String?
+}
