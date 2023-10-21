296c903c57be659f8373386282a9fd6d1977cb71
Banah Abdeljaber <babdeljaber14@gmail.com>
2023-10-21 18:36:31 -0500

    Added swift file to filter news stories

---
+++ Category.swift
@@ -0,0 +1,29 @@
+//
+//  Category.swift
+//  edUcate
+//
+//  Created by Banah Daniella Abdeljaber on 10/21/23.
+//
+
+import Foundation
+
+enum Category: String, CaseIterable {
+    case general
+    case business
+    case technology
+    case entertainment
+    case sports
+    case science
+    case health
+    
+    var text: String {
+        if self == .general {
+            return "Top Headlines"
+        }
+        return rawValue.capitalized
+    }
+}
+
+extension Category: Identifiable {
+    var id: Self { self }
+}
